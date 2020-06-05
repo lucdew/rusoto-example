0927933dcedc1f4eefc4e13acdba272e1ebf2ef4
ldewavri <luc.dewavrin@gemalto.com>
2020-06-05 09:46:21 +0200

    Rename binary to more meaningful name Disable connection pooling due to connection closed error

--- src/client.rs
+++ src/client.rs
@@ -1,5 +1,4 @@
 use anyhow::Result;
-use core::time::Duration;
 use hyper::{Client, Uri};
 use hyper_proxy::{Intercept, Proxy, ProxyConnector};
 use hyper_tls::HttpsConnector;
@@ -20,9 +19,15 @@ pub fn new_client() -> Result<HttpClient> {
         http_connector = ProxyConnector::new(connector)?;
     }
     let mut hyper_builder = Client::builder();
+
+    // disabling due to connection closed issue
+    hyper_builder.pool_max_idle_per_host(0);
+    /* still getting connection closed before message completed
+     * https://github.com/rusoto/rusoto/issues/1766
     hyper_builder
-        .pool_idle_timeout(Duration::from_secs(60))
-        .pool_max_idle_per_host(5);
+        .pool_idle_timeout(Duration::from_secs(5))
+        .pool_max_idle_per_host(4)
+        .retry_canceled_requests(true);*/
     Ok(rusoto_core::HttpClient::from_builder(
         hyper_builder,
         http_connector,
